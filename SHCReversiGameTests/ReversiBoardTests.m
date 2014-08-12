8af021a78c8e2b97c68847f9f6b76b944a86f8c6
Paul Crowell <kennebecstars@gmail.com>
2014-08-12 18:57:28 -0400

    Local ci.

--- SHCReversiGameTests/ReversiBoardTests.m
+++ SHCReversiGameTests/ReversiBoardTests.m
@@ -40,9 +40,9 @@
                                   @"Making a move in the pre-game state should do nothing");
 }
 
-- (void)test_iLikeBreakingBuildsAndICannotLie
-{
-    XCTAssertTrue(false, @"The integration will fail");
-}
+//- (void)test_iLikeBreakingBuildsAndICannotLie
+//{
+//    XCTAssertTrue(false, @"The integration will fail");
+//}
 
 @end
